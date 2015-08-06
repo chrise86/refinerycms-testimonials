e49153a51e649c920335c95ccc82bab396c18b08
Chris Edwards <chris@chrise.net>
2015-08-06 17:33:04 +0100

    updated model to compensate for website

--- app/models/refinery/testimonials/testimonial.rb
+++ app/models/refinery/testimonials/testimonial.rb
@@ -5,21 +5,29 @@ module Refinery
 
       # Constants for how to show the testimonials
       ORDER = %w[Random Recent]
-      CHANNELS = %w[Letter Email Facebook Twitter]
+      CHANNELS = %w[Letter Email Facebook Twitter Website]
 
       CHANNELS.each_with_index do |meth, index|
         define_method("#{meth}?") { channels == index }
       end
 
-       acts_as_indexed :fields => [:name, :company]
+      acts_as_indexed :fields => [:name, :company]
 
-      validates :name, :presence => true #, :uniqueness => true
-      validates :quote, :presence => true
+      validates :name, presence: true #, :uniqueness => true
+      validates :quote, presence: true
+      validates :email, presence: true, if: :via_website?
+
+      scope :random, -> limit { where('id >= ?', rand(count)).limit(limit) }
+      scope :approved, -> { where(approved: true) }
 
       def flash_name
         "Quote by #{self.name}"
       end
 
+    private
+      def via_website?
+        received_channel == 'Website'
+      end
     end
   end
 end
